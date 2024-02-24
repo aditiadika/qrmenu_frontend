e00cd26124d6bdca729650c0278d64f8aba30443
Dika Aditia <aditiaaadicka@gmail.com>
2024-02-24 16:10:30 +0700

    settingup react and react router for frontend

---
+++ src/router/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+        </Switch>
+    </BrowserRouter>
+  )
+}
